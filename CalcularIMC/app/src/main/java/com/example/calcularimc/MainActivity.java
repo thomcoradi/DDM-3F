219e43e0e057b5e752e27a840449866e8faa59c2
Thomas Coradi <125597694+thomcoradi@users.noreply.github.com>
2025-02-26 15:01:09 -0300

    Priemra Aula 26/02/25

---
+++ CalcularIMC/app/src/main/java/com/example/calcularimc/MainActivity.java
@@ -0,0 +1,20 @@
+private TextView txtResultado;
+
+package com.example.calcularimc;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public void {
+
+        }
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
